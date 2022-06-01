3c47503e8f1119be74d35867322afa2d2d0af5db
Lone Wolf <53178226+Lone-Wolf17@users.noreply.github.com>
2022-06-01 11:47:13 +0100

    ** Improve HomePage Screen  * Display a Grid List of Features on Home Page

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -4,10 +4,11 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterfeaturesapp/ui/Utils/drawer_util.dart';
 
+import '../Utils/route_names.dart';
 import 'dynamic_theming.dart';
 
 class HomePage extends StatelessWidget {
-  var themeData;
+  final themeData;
 
   HomePage(this.themeData);
 
@@ -30,10 +31,93 @@ class HomePage extends StatelessWidget {
         ],
       ),
       drawer: DrawerUtil(),
-      body: Center(
-        child: Text('Open Drawer to checkout different features',
-            style: TextStyle(fontSize: 15.0)),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 12),
+        child: GridView(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3, crossAxisSpacing: 12, mainAxisSpacing: 8),
+          children: [
+            _GridTile(
+                icon: Icons.camera,
+                title: 'Dynamic Theming',
+                navigateTo: RouteNames.dynamicThemingScreen),
+            _GridTile(
+                icon: Icons.arrow_forward_rounded,
+                title: 'Swipe to Dismiss',
+                navigateTo: RouteNames.swipeToDismissScreen),
+            _GridTile(
+                icon: Icons.keyboard_double_arrow_up_rounded,
+                title: 'Bottom Sheets',
+                navigateTo: RouteNames.bottomSheetsScreen),
+            _GridTile(
+                icon: Icons.table_chart,
+                title: 'TabBar Layout',
+                navigateTo: RouteNames.tabBarLayoutScreen),
+            _GridTile(
+                icon: Icons.network_check_rounded,
+                title: 'Connectivity Test',
+                navigateTo: RouteNames.connectivityTestScreen),
+            _GridTile(
+                icon: Icons.flip,
+                title: 'PageView Demo',
+                navigateTo: RouteNames.pageViewDemoScreen),
+            _GridTile(
+                icon: Icons.flip_camera_android_rounded,
+                title: 'Circular Pan Wheel',
+                navigateTo: RouteNames.circularPanWheelScreen),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class _GridTile extends StatelessWidget {
+  const _GridTile(
+      {Key? key,
+      required this.icon,
+      required this.title,
+      required this.navigateTo})
+      : super(key: key);
+
+  final IconData icon;
+  final String title;
+
+  /// The Route Name of the screen to navigate to
+  final String navigateTo;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.pushNamed(context, navigateTo);
+      },
+      child: Container(
+          padding: EdgeInsets.all(6),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              gradient: LinearGradient(
+                  begin: Alignment.centerLeft,
+                  end: Alignment.topRight,
+                  colors: [
+                    Colors.primaries[
+                        Random().nextInt(Colors.primaries.length - 1)],
+                    Colors.accents[Random().nextInt(Colors.accents.length - 1)],
+                  ]),
+              color: Colors
+                  .primaries[Random().nextInt(Colors.primaries.length - 1)]),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(icon, size: 20, color: Colors.white),
+              SizedBox(height: 8),
+              Text(title,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontWeight: FontWeight.w600, color: Colors.white))
+            ],
+          )),
     );
   }
 }
